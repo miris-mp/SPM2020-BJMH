b8eece2e9c83b1bf0966ce4d562efdb868ec3c65
@miris-mp miris-mp <miriam.petrocchi94@gmail.com>
2020-12-18 07:35:59 +0100

    delete all and add only the skeleton of the app(bkend and frtend)

--- src/main/java/spm/bjmh/SPM2020BJMH/Enum/ParkingAvailability.java
+++
@@ -1,8 +0,0 @@
-package spm.bjmh.SPM2020BJMH.Enum;
-
-public enum ParkingAvailability {
-    AVAILABLE,
-    NOT_AVAILABLE,
-    BOOKED,
-    DISABLED
-}
--- src/main/java/spm/bjmh/SPM2020BJMH/Enum/Roles.java
+++
@@ -1,9 +0,0 @@
-package spm.bjmh.SPM2020BJMH.Enum;
-
-public enum Roles {
-    ROLE_ADMIN,
-    ROLE_POLICEMAN,
-    ROLE_PARKING_MANAGER,
-    ROLE_DRIVER,
-    ROLE_TOW_TRUCK
-}
